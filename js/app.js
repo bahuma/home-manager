75af8a5aec25d0223726f3c6c1f0a81b343f5b91
Maximilian Bachhuber <bahuma20@bahuma-online.de>
2014-12-23 16:49:18 +0000

    Changed tasks

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
-var app = angular.module("HomePlanner", ['gapi', 'ngRoute', 'ngAnimate', 'ngMaterial', 'angular-flexslider']);
+var app = angular.module("HomePlanner", [
+    'gapi', 
+    'ngRoute', 
+    'ngAnimate', 
+    'ngMaterial', 
+    'angular-flexslider'
+]);
 
 app.value('GoogleApp', {
     apiKey: 'JYMy1-rQXJXmZb18jHW_TYjQ',
